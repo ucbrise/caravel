d7caf23383112d266ab64550def3b42911608ca9
Simon Mo <xmo@berkeley.edu>
2018-10-13 23:32:56 +0000

    Download res50 ckpt

--- setup.sh
+++ setup.sh
@@ -1,1 +1,6 @@
 git clone http://github.com/tensorflow/models.git
+
+# Download res50 check point
+wget http://download.tensorflow.org/models/resnet_v1_50_2016_08_28.tar.gz
+tar xzf resnet_v1_50_2016_08_28.tar.gz
+rm resnet_v1_50_2016_08_28.tar.gz
