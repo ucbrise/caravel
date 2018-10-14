99e1d002dfa2b6964231c18252c52bc1fd5f2f91
Simon Mo <xmo@berkeley.edu>
2018-10-14 00:56:07 +0000

    Add setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
-git clone http://github.com/tensorflow/models.git
+#git clone http://github.com/tensorflow/models.git
+
+mkdir ckpts
+cd ckpts
 
 # Download res50 check point
 wget http://download.tensorflow.org/models/resnet_v1_50_2016_08_28.tar.gz
 tar xzf resnet_v1_50_2016_08_28.tar.gz
 rm resnet_v1_50_2016_08_28.tar.gz
+
+wget http://download.tensorflow.org/models/resnet_v1_152_2016_08_28.tar.gz
+tar xzf resnet_v1_152_2016_08_28.tar.gz
+rm resnet_v1_152_2016_08_28.tar.gz
+
+wget https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.0_96.tgz
+tar -xvf mobilenet_v2_1.0_96.tgz
+rm  mobilenet_v2_1.0_96.tgz
+
+cd ..
